88822f7c61a6ea52a12e4f2d956e64c08c122ff6
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-10-21 22:08:19 +0600

    ultra

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         }
       </ul>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
